3690775e1779a633361e6cbd6308d384ce7f1c23
suzieep <wnsuwls0707@gmail.com>
2021-06-21 15:47:41 +0900

    #2.4 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 
-function Food({name,picture}){
+function Food({name,picture,rating}){
   return <div>
     <h3>I love {name}</h3>
+    <h4>rating = {rating}/5.0 </h4>
     <img src={picture} alt={name} />
     </div>
   ;
@@ -16,23 +18,33 @@ const foodILike = [
   },
   { id:2,
     name:"b",
-    image:"https://source.unsplash.com/category/nature/1600x900"
+    image:"https://source.unsplash.com/category/nature/1600x900",
+    rating:4.9
   },
   {
     id:3,
     name:"c",
-    image:"https://source.unsplash.com/category/nature/1600x900"
+    image:"https://source.unsplash.com/category/nature/1600x900",
+    rating:4.7
   },
 ];
-// function renderFood(dish){
-//   console.log(dish);
-//   return <Food name={dish.name} picture={dish.image} />
-// }
+
+
+Food.propTypes={
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+}
+
 function App() {
   return (
     <div>
       {foodILike.map(dish =>(
-        <Food key={dish.id} name={dish.name} picture={dish.image}/>
+        <Food 
+          key={dish.id} 
+          name={dish.name} 
+          rating={dish.rating}
+          picture={dish.image}/>
       ))}
     </div>
   );
