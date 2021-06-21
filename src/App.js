6000dcb856a8b2169d12a298580e237e2f0b126a
suzieep <wnsuwls0707@gmail.com>
2021-06-21 15:27:56 +0900

    #2.3 map Recap

--- src/App.js
+++ src/App.js
@@ -4,30 +4,35 @@ import React from "react";
 function Food({name,picture}){
   return <div>
     <h3>I love {name}</h3>
-    <img src={picture} />
+    <img src={picture} alt={name} />
     </div>
   ;
 
 }
 const foodILike = [
-  {
+  { id:1,
     name:"a",
     image:"https://source.unsplash.com/category/nature/1600x900"
   },
-  {
+  { id:2,
     name:"b",
     image:"https://source.unsplash.com/category/nature/1600x900"
   },
   {
+    id:3,
     name:"c",
     image:"https://source.unsplash.com/category/nature/1600x900"
   },
-]
+];
+// function renderFood(dish){
+//   console.log(dish);
+//   return <Food name={dish.name} picture={dish.image} />
+// }
 function App() {
   return (
     <div>
-      {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image}/>
+      {foodILike.map(dish =>(
+        <Food key={dish.id} name={dish.name} picture={dish.image}/>
       ))}
     </div>
   );
