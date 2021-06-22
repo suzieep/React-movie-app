7221d3ee1a515b364270bc0d8989dc9a091e3c23
suzieep <wnsuwls0707@gmail.com>
2021-06-22 15:56:53 +0900

    lec fin

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 import Navigation from "./components/Navigation"
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
     </HashRouter>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,23 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
+import { Link } from "react-router-dom";
 
 function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <div className="movie">
-      <img src={poster} alt={title} title={title} />
-      <div className="movie_data" />
-      <h3 className="movie__title" style={{ backgroundColor: "royalblue" }}>
-        {title}
-      </h3>
-      <h5 className="movie__year">{year}</h5>
-      <ul className="genres">
-        {genres.map((genre, index) => (
-          <li key={index} className="genres__genre">{genre}</li>
-        ))}
-      </ul>
-      <p className="movie__summary">{summary.slice(0,180)}...</p>
-    </div>
+    <Link
+      to={{
+        pathname: `/movie/${id}`,
+        state: {
+          year,
+          title,
+          summary,
+          poster,
+          genres,
+        },
+      }}
+    >
+      <div className="movie">
+        <img src={poster} alt={title} title={title} />
+        <div className="movie_data" />
+        <h3 className="movie__title" style={{ backgroundColor: "royalblue" }}>
+          {title}
+        </h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
+      </div>
+    </Link>
   );
 }
 Movie.propTypes = {
